7eb3f05e1a562f7fe8652b7be699aa3236b97666
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2024-01-27 11:22:00 -0300

    images

--- src/components/LoMasVendido.jsx
+++ src/components/LoMasVendido.jsx
@@ -17,47 +17,47 @@ const LoMasVendido = (props) => {
                     <h1 className='text-left'>Lo Más Vendido</h1>
                 </Container>
                 <Container fluid className='d-flex justify-content-center flex-wrap'>
-                    <Card className='mb-4 mx-2' style={{ width: '18rem', height:'22rem' }}>
-                        <Card.Img variant="top" src="https://media.discordapp.net/attachments/1176712497668030486/1182142685528727613/imagen_3.png?ex=6596142b&is=65839f2b&hm=bf57c356bba754f3c7c9887cddd13dd264404efc9b06cd0c6fd36d948b6142c6&=&format=webp&quality=lossless" />
+                    <Card className='mb-4 mx-2' style={{ width: '18rem', height: '22rem' }}>
+                        <Card.Img variant="top" src="https://media.discordapp.net/attachments/1176712497668030486/1200699909762986004/Diseno_sin_titulo.png?ex=65c721ee&is=65b4acee&hm=6abcf7e51994d611b85d75a3d390875bca3d576e9462f46e3991320195ab7f67&=&format=webp&quality=lossless" />
                         <Card.Body className='text-center custom-card-body'>
                             <Card.Title>Call Of Duty III</Card.Title>
                             <Card.Text>
                                 <i className="bi bi-tag-fill"></i> $35.000
                             </Card.Text>
-                           
+
                         </Card.Body>
                     </Card>
 
-                    <Card className='mb-4 mx-2' style={{ width: '18rem', height:'22rem' }}>
-                        <Card.Img variant="top" src="https://media.discordapp.net/attachments/1176712497668030486/1182143216087220344/imagen_11.png?ex=659614a9&is=65839fa9&hm=b2ef80946fef5df87fac27cf8cddcaa4f1fd2691aab5740a1d58a8e130385cac&=&format=webp&quality=lossless" />
+                    <Card className='mb-4 mx-2' style={{ width: '18rem', height: '22rem' }}>
+                        <Card.Img variant="top" src="https://media.discordapp.net/attachments/1176712497668030486/1200699865051701379/Diseno_sin_titulo_10.png?ex=65c721e3&is=65b4ace3&hm=37be5ce036b6e5c19b08a6fbc5b8262bbfc3ee1aff4a963431dde17298205d51&=&format=webp&quality=lossless" />
                         <Card.Body className='text-center custom-card-body'>
                             <Card.Title>TLOR: Returns To Moria</Card.Title>
                             <Card.Text>
                                 <i className="bi bi-tag-fill"></i> $22.500
                             </Card.Text>
-                            
+
                         </Card.Body>
                     </Card>
 
-                    <Card className='mb-4 mx-2' style={{ width: '18rem', height:'22rem' }}>
-                        <Card.Img variant="top" src="https://media.discordapp.net/attachments/1176712497668030486/1182143216561172600/imagen_16.png?ex=659614a9&is=65839fa9&hm=f6c402b1a487ef3370308f5147326ca8cbeb60bf5261c26320048e82a46dc688&=&format=webp&quality=lossless" />
+                    <Card className='mb-4 mx-2' style={{ width: '18rem', height: '22rem' }}>
+                        <Card.Img variant="top" src="https://media.discordapp.net/attachments/1176712497668030486/1200699864758091868/Diseno_sin_titulo_11.png?ex=65c721e3&is=65b4ace3&hm=5e0da2ca886bb5addddd83c0fc8bc255e640e4bdd80f56c79427c88a7eebd6ee&=&format=webp&quality=lossless" />
                         <Card.Body className='text-center custom-card-body'>
                             <Card.Title>Red Dead Redemption</Card.Title>
                             <Card.Text>
                                 <i className="bi bi-tag-fill"></i> $13.000
                             </Card.Text>
-                            
+
                         </Card.Body>
                     </Card>
 
-                    <Card className='mb-4 mx-2' style={{ width: '18rem', height:'22rem' }}>
-                        <Card.Img variant="top" src="https://cdn.discordapp.com/attachments/1176712497668030486/1182143217823645796/imagen_13.png?ex=659f4f2a&is=658cda2a&hm=14cbcd737dccd573f53b6300101ee858ddb6c045739dd206b7f7d324575b19bb&" />
+                    <Card className='mb-4 mx-2' style={{ width: '18rem', height: '22rem' }}>
+                        <Card.Img variant="top" src="https://media.discordapp.net/attachments/1176712497668030486/1200699863839539210/Diseno_sin_titulo_14.png?ex=65c721e3&is=65b4ace3&hm=49e033838dace9f83b8aa7119e83598ec474f6057ba5a063d09458fa93a19e20&=&format=webp&quality=lossless" />
                         <Card.Body className='text-center custom-card-body'>
                             <Card.Title>Risk of Rain</Card.Title>
                             <Card.Text className='custom-card-text'>
                                 <i className="bi bi-tag-fill"></i> $3.000
                             </Card.Text>
-                            
+
                         </Card.Body>
                     </Card>
                 </Container>
